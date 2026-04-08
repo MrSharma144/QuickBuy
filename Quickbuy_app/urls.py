680050ac7bb7e2cdde8f32ff04222afe25e7999d
Prateek <prateekps0123@gmail.com>
2026-04-08 12:41:53 +0530

    Add product display page with quantity inputs and orders list

--- Quickbuy_app/urls.py
+++ Quickbuy_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('create-checkout-session/', views.create_checkout_session, name='create_checkout_session'),
+    path('payment/success/', views.payment_success, name='payment_success'),
+    path('payment/cancel/', views.payment_cancel, name='payment_cancel'),
 ]
--- Quickbuy_app/views.py
+++ Quickbuy_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
+from django.http import HttpResponse
 from .models import Product, Order
 
 
@@ -6,3 +8,19 @@ def home(request):
     products = Product.objects.all()
     orders = Order.objects.filter(status='paid').order_by('-created_at')
     return render(request, 'home.html', {'products': products, 'orders': orders})
+
+
+@require_POST
+def create_checkout_session(request):
+    # Will be implemented in Step 6
+    return HttpResponse("Stripe checkout coming in Step 6", status=501)
+
+
+def payment_success(request):
+    # Will be implemented in Step 7
+    return HttpResponse("Payment success coming in Step 7", status=501)
+
+
+def payment_cancel(request):
+    # Will be implemented in Step 7
+    return redirect('home')
